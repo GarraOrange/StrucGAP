4195d169aa288183637f3eefbed7d085d95b3f72
Garra <141489300+GarraOrange@users.noreply.github.com>
2026-04-26 04:34:33 +0800

    Create phant.sh

---
+++ deer/phant.sh
@@ -0,0 +1,36 @@
+$TOOLS_DIR = "D:\Research\tools"
+$PHANTOM_ZIP = "$TOOLS_DIR\phantomjs-2.1.1-windows.zip"
+$PHANTOM_DIR = "$TOOLS_DIR\phantomjs-2.1.1-windows"
+$PHANTOM_BIN = "$PHANTOM_DIR\bin"
+
+New-Item -ItemType Directory -Force $TOOLS_DIR | Out-Null
+
+$urls = @(
+    "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-windows.zip",
+    "https://mirrors.huaweicloud.com/phantomjs/phantomjs-2.1.1-windows.zip"
+)
+
+foreach ($url in $urls) {
+    try {
+        Write-Host "Trying download:" $url
+        Invoke-WebRequest -Uri $url -OutFile $PHANTOM_ZIP
+        break
+    } catch {
+        Write-Host "Failed:" $url
+    }
+}
+
+if (!(Test-Path $PHANTOM_ZIP)) {
+    throw "PhantomJS download failed."
+}
+
+Expand-Archive -Force $PHANTOM_ZIP -DestinationPath $TOOLS_DIR
+
+$env:Path = "$PHANTOM_BIN;$env:Path"
+
+$userPath = [Environment]::GetEnvironmentVariable("Path", "User")
+if ($userPath -notlike "*$PHANTOM_BIN*") {
+    [Environment]::SetEnvironmentVariable("Path", "$PHANTOM_BIN;$userPath", "User")
+}
+
+phantomjs --version
